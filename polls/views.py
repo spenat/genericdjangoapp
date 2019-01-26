780612d1021a7f0c43d662fd816d429290b34c97
Mikael Andrén <mikaandr@gmail.com>
2019-01-26 23:27:25 +0100

    pjuh

--- polls/views.py
+++ polls/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
+
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world")
     #return render("Hello world")
+
+
+def health(request):
+    return HttpResponse("I'm healthy yo")
--- thesite/urls.py
+++ thesite/urls.py
@@ -15,9 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+from django.conf import settings
+from polls.views import health
 
 
 urlpatterns = [
     path('', include('polls.urls')),
     path('admin/', admin.site.urls),
+    url(r'^health$', health),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
